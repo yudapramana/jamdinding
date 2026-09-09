7317f817a1b702b8b725ef04c927484171ee7d02
pramana yuda <pramanayuda772@gmail.com>
2026-09-09 14:01:57 +0700

    push

--- app/Http/Controllers/API/V1/EventParticipantController.php
+++ app/Http/Controllers/API/V1/EventParticipantController.php
@@ -793,7 +793,7 @@ class EventParticipantController extends Controller
 
         $event = Event::findOrFail($request['event_participant']['event_id']);
 
-        if (!$event->isStageActive('persiapan') || !$event->isStageActive('pendaftaran')) {
+        if (!$event->isStageActive('persiapan') && !$event->isStageActive('pendaftaran')) {
             return response()->json([
                 'message' => 'Tahap persiapan/pendaftaran belum dimulai atau sudah berakhir.'
             ], 403);
